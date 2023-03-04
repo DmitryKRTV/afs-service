4047aff91d5d4847870342c7e064101c2b7c7b58
dmitrykrtv <dmitry.korotayev@gmail.com>
2023-03-04 15:00:23 +0300

    feat(client): interceptor

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { AuthService } from './core/services/auth.service'
 
 @Component({
   selector: 'afs-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {
-  title = 'client'
+export class AppComponent implements OnInit {
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    const tempToken = localStorage.getItem('auth-token')
+    if (tempToken) {
+      this.authService.setToken(tempToken)
+    }
+  }
 }
