248f83b5780bae51ea581fce0c2d1b4da4f9ea5a
dmitrykrtv <dmitry.korotayev@gmail.com>
2023-03-14 15:38:02 +0300

    fix(server): vercel problems

--- server/src/controllers/auth-controller.ts
+++ server/src/controllers/auth-controller.ts
@@ -1,46 +1,45 @@
-import { Request, Response } from "express"
-import { User } from "../models/User.model"
-import bcrypt from "bcryptjs"
-import jwt from "jsonwebtoken"
-import { TokenType } from "../models/Token.model"
-import { errorHandler } from "../utils/errorHandler"
+import { Request, Response } from 'express'
+import { User } from '../models/User.model'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import { TokenType } from '../models/Token.model'
+import { errorHandler } from '../utils/errorHandler'
 
 export const authController = {
   async login(req: Request, res: Response) {
     const candidate = await User.findOne({
-      email: req.body.email,
+      email: req.body.email
     })
+    console.log(candidate)
+    console.log(process.env.JWT)
     if (candidate) {
       const passwordResult = bcrypt.compareSync(req.body.password, candidate.password)
       if (passwordResult) {
         const token = jwt.sign({
           email: candidate.email,
           userId: candidate._id
-        } as TokenType, process.env.JWT || "AFS", { expiresIn: 3600 })
-
+        } as TokenType, process.env.JWT || 'AFS', { expiresIn: 3600 })
         res.status(200).json({
           token: `Bearer ${token}`
         })
-
       } else {
         res.status(401).json({
-          message: "User not found"
+          message: 'User not found'
         })
       }
     } else {
       res.status(404).json({
-        message: "User not found"
+        message: 'User not found'
       })
     }
   },
-
   async register(req: Request, res: Response) {
     const candidate = await User.findOne({
-      email: req.body.email,
+      email: req.body.email
     })
     if (candidate) {
       res.status(409).json({
-        message: "The email is already registered",
+        message: 'The email is already registered'
       })
     } else {
       const salt = bcrypt.genSaltSync(10)
